0bd26d2e6a2de972e63ac7bed77eda7192853f2a
Elizaspr <spratt.eliza@gmail.com>
2026-04-07 10:29:39 -0400

    IC

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("apricot");
+        System.out.println("peach");
+        System.out.println("mango");
+        System.out.println("lime");
+        System.out.println("egg");
+        System.out.println("milk");
+        System.out.println("cheese");
+        System.out.println("ice cream");
+        System.out.println("apple");
+        System.out.println("bread");
+    }
+    }
