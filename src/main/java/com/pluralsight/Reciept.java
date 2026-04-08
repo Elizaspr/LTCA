2b194f57bb3e0f886b3bb47613a9aca8683ea49e
Elizaspr <spratt.eliza@gmail.com>
2026-04-08 15:29:25 -0400

    Exercise 2

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main (String [] args) {
+
+        //Question 1:
+        String item = "Canon Battery";
+        double price = 59.99;
+
+        // Question 2:
+        int quantity = 1;
+
+        // Question 3
+
+        System.out.println("You bought " +quantity + " " + item + " for " +price);
+
+
+
+    }
+}
+
---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+
+        // Question 1:
+        String favColor = "orange";
+        System.out.println("My favorite color is: "  + favColor);
+        System.out.println();
+
+        //Question 2:
+        int yearStarted = 2026;
+        System.out.println("I started the class in the year " +yearStarted);
+        System.out.println();
+
+        // Question 3:
+        char middleInitial = 'S';
+        System.out.println("My middle initial is: " + middleInitial);
+        System.out.println();
+
+        // Question 4
+        boolean has_pets = true;
+        System.out.println("The response to 'Do I have pets' is: " + has_pets);
+        System.out.println();
+
+
+        //Question 5
+        String nice = "Get well soon!";
+        System.out.println(nice);
+        System.out.println();
+
+
+        // STEP 2
+
+        int daysInweek = 7 ;
+
+        System.out.println("There are " + daysInweek + " in a week");
+        System.out.println();
+
+        double coffeePrice = 4.99;
+        System.out.println("Coffee costs " +coffeePrice);
+        System.out.println();
+
+        char favLetter = 'L';
+        System.out.println("My favorite letter is "+favLetter);
+
+        boolean isRaining = false;
+        System.out.println ("The answer to the boolean question 'is it raining?' is : " + isRaining);
+
+
+
+
+    }
+}
